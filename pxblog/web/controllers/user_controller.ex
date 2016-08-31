186e2f6811f32039842e8ee060f1ebfb0ad1b368
Richard Tan <richard.tan@aggregateiq.com>
2016-08-31 09:14:01 -0700

    Fixed tests

--- pxblog/web/controllers/user_controller.ex
+++ pxblog/web/controllers/user_controller.ex
@@ -3,14 +3,38 @@ defmodule Pxblog.UserController do
 
   alias Pxblog.User
 
+  plug :scrub_params, "user" when action in [:create]
+
+  plug :authorize_admin when action in [:new, :create]
+  plug :authorize_user when action in [:edit, :update, :delete]
+
+  defp authorize_admin(conn, _) do
+    user = get_session(conn, :current_user)
+    if user && Pxblog.RoleChecker.is_admin?(user) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not authorized to create new users!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
+  defp authorize_user(conn, _) do
+    user = get_session(conn, :current_user)
+    if user && (Integer.to_string(user.id) == conn.params["id"] || Pxblog.RoleChecker.is_admin?(user)) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not authorized to modify that user!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
   def index(conn, _params) do
     users = Repo.all(User)
     render(conn, "index.html", users: users)
-  end
-
-  def new(conn, _params) do
-    changeset = User.changeset(%User{})
-    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
@@ -26,15 +50,32 @@ defmodule Pxblog.UserController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def delete(conn, %{"id" => id}) do
     user = Repo.get!(User, id)
-    render(conn, "show.html", user: user)
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(user)
+
+    conn
+    |> put_flash(:info, "User deleted successfully.")
+    |> redirect(to: user_path(conn, :index))
   end
 
   def edit(conn, %{"id" => id}) do
     user = Repo.get!(User, id)
     changeset = User.changeset(user)
     render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    render(conn, "show.html", user: user)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
@@ -49,17 +90,5 @@ defmodule Pxblog.UserController do
       {:error, changeset} ->
         render(conn, "edit.html", user: user, changeset: changeset)
     end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    user = Repo.get!(User, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(user)
-
-    conn
-    |> put_flash(:info, "User deleted successfully.")
-    |> redirect(to: user_path(conn, :index))
   end
 end
